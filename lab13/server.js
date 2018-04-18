ce80ad60ca9e1c1e536bb824398f7e5504fc9416
eGathergood <ewan_good@hotmail.co.uk>
2018-04-18 19:34:08 +0100

    Route updates

--- lab13/server.js
+++ lab13/server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var knockknock = require ('knock-knock-jokes');
 var app = express();
 
 app.get('/', function(req, res){
   res.send("This works i think - express");
+});
+
+app.get('/', function(req, res){
+  res.send("Route 2");
+});
+
+app.get('/', function(req, res){
+  var randomJoke = knockknock()
+  res.send(randomJoke);
 });
 
 app.listen(8080);
