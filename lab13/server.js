386e851293ed804a455beccb64b75013770083b4
eGathergood <ewan_good@hotmail.co.uk>
2018-04-18 19:49:38 +0100

    test

--- lab13/server.js
+++ lab13/server.js
@@ -16,8 +16,8 @@ app.get('/joke', function(req, res){
 });
 
 app.get('/add', function(req, res){
-  var x = parseInt(5);
-  var y = parseInt(5;
+  var x = parseInt(req.query.x);
+  var y = parseInt(req.query.y);
   res.send("X + Y=" +(x+y));
 
 });
