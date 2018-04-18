8c3736f93bf94e052abf4260d4eed0da5ada4839
eGathergood <ewan_good@hotmail.co.uk>
2018-04-18 23:00:54 +0100

    ..

--- lab15/server.js
+++ lab15/server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 
 var db;
 
-MongoClient.connect(url, function(err, databse){
+MongoClient.connect(url, function(err, database){
   if (err) throw err;
   db = database;
   app.listen(8080);
