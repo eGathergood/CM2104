c2dbbcce942d1b75777b89cca58826b8e0b6e0ce
eGathergood <ewan_good@hotmail.co.uk>
2018-04-18 20:23:43 +0100

    fix

--- lab13/server.js
+++ lab13/server.js
@@ -8,7 +8,7 @@ app.get('/getForm', function(reqs, res){
   var name = req.query.name;
   var quest = req.query.quest;
     res.send("Hi " +name+" I am sure you will" + quest);
-})
+});
 
 app.get('/', function(req, res){
   res.send("This works i think - express");
