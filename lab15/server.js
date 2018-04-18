7e2246233c6803e3de72fa47be74ee465b91dcbc
eGathergood <ewan_good@hotmail.co.uk>
2018-04-18 23:16:07 +0100

    route

--- lab15/server.js
+++ lab15/server.js
@@ -3,7 +3,11 @@ const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+
 app.use(express.static('public'))
+
+app use(bodyParser.urlencoded({extended:true}))
 
 var db;
 
@@ -28,3 +32,11 @@ app.get('/all', function(req, res){
     res.send(output);
   });
 });
+
+app.post('/quotes', function (req, res){
+  db.collection('quotes').save(req.body, function(err, result){
+    if (err) throw err;
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
